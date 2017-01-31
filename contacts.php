4b7902cd6be0b137c7d471d8b4d450a712696fe1
LucasGou <lululucas@hotmail.fr>
2017-01-31 17:16:02 +0100

    Add files via upload

--- contacts.php
+++ contacts.php
@@ -14,23 +14,23 @@
 </head>
 <body>
 	<div id='header'>
-        <a href="index.html"><img height="157" src="ulcobet.png" width="207" /></a>
+        <a href="index.php"><img height="157" src="ulcobet.png" width="207" /></a>
 		        <input type="text" placeholder="Nom de compte" name="userid" id="userid" class="userid"/>
         <input type="password" placeholder="Mot de passe" name="pass" id="pass" class="pass" /> 
         <button class="connex" >Se connecter </button>
-        <button class="inscri" value="inscription.html">Inscription </button>
+        <button class="inscri" value="inscription.php">Inscription </button>
 	</div>
 
 	<div class="container">
 		<ul id="nav" class="myTopnav">
 			<li><a></a></li>
-			<li><a href="index.html">Accueil</a></li>
-			<li><a href="parisencemoment.html">En ce moment</a>
-			<li><a href="parisresultats.html">Résultats</a>
-			<li><a href="mesparis.html">Mes paris</a></li>
-			<li><a href="contacts.html">Contact</a></li>
-			<li><a href="creationpari.html">Créer un pari</a></li>
-			<li><a href="propositiongage.html">Proposer un gage</a></li>
+			<li><a href="index.php">Accueil</a></li>
+			<li><a href="parisencemoment.php">En ce moment</a>
+			<li><a href="parisresultats.php">Résultats</a>
+			<li><a href="mesparis.php">Mes paris</a></li>
+			<li><a href="contacts.php">Contact</a></li>
+			<li><a href="creationpari.php">Créer un pari</a></li>
+			<li><a href="propositiongage.php">Proposer un gage</a></li>
 			<li class="icon"><a href="javascript:void(0);" onclick="myFunction()">&#9776;</a></li>
 		</ul>
 </div>
